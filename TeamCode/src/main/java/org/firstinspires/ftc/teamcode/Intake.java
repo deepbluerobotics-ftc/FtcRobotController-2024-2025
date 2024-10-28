3c5d4291ba8b5dea2d87f28c6117e216d5979aa7
lahp5 <lahp5@schls.albco>
2024-10-28 17:36:29 -0400

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Intake", group="Linear OpMode")
+
+public class Intake extends LinearOpMode {
+    private Servo intakeWheel = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor intake = null;
+
+    @Override
+    public void runOpMode() {
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        intakeWheel = hardwareMap.get(Servo.class, "intake_wheel");
+        intakeWheel.setDirection(Servo.Direction.REVERSE);
+        intakeWheel.setPosition(0);
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+            double intakePower = 0;
+            if (gamepad1.dpad_right){
+                intakePower = 0.7;
+            }else if (gamepad1.dpad_left) {
+                intakePower = -0.7;
+            }else {
+                intakePower = 0;
+            }
+
+            intake.setPower(intakePower);
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("intake", "%4.2f, %4.2f", intakePower);
+
+            telemetry.update();
+        }
+    }}
