4b3213f37a473dc7e6642018a3f48cbfa734027e
brwu3 <brwu3@schls.albco>
2025-01-13 16:28:46 -0500

    Maybe added toggle. Added test file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoAutoTest.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="AutoServoTest", group="Linear OpMode")
+
+public class servoAutoTest extends LinearOpMode{
+    private ElapsedTime runtime;
+
+    public static Servo platform = null;
+
+    @Override
+    public void runOpMode() {
+        runtime = new ElapsedTime();
+
+        platform     = hardwareMap.get(Servo.class, "platform");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        platform.setPosition(1);
+        
+    }
+}
